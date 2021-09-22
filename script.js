84da1383fa28e7c9ac7cb8bfca1d61b60907a844
pinky <pinky.fardous@gmail.com>
2021-09-23 00:35:38 +0200

    add more lights

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 // var myBtn1 = document.getElementById("btn1");
 // var myBtn2 = document.getElementById("btn2");
 var changeHeadline = document.getElementsByTagName("h3")[0];
-var lightImage1 = document.getElementsByClassName("light1")[0];
-var lightImage2 = document.getElementsByClassName("light2")[1];
-var lightImage3 = document.getElementsByClassName("light3")[2];
 const toggleBtn = document.getElementById('toggle');
 let lightState = 'off';
 
 //Function for light on and light off.
     
 function turnOn(){
-    lightImage1.src = "images/lightOn.jpg";
+    let lights = document.getElementsByClassName("light");;
+    for (let i = 0; i < 3; i++) {
+        lights[i].src = "images/lightOff.jpg";    
+    }
     changeHeadline.innerHTML = "On the light";
     changeHeadline.setAttribute("style","color:blue");
     toggleBtn.innerHTML = "On";
 }
 
 function turnOff() {
-    lightImage1.src = "images/lightOff.jpg";
+    let lights = document.getElementsByClassName("light");;
+    for (let i = 0; i < 3; i++) {
+        lights[i].src = "images/lightOn.jpg";    
+    }
     changeHeadline.innerHTML = "Off the light";
     changeHeadline.setAttribute("style","color:red");
     toggleBtn.innerHTML = "Off";
